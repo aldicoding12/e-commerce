c72afb8d5def04d414763f7241eeef91cbd37a61
Alcoding <102742081+aldicoding12@users.noreply.github.com>
2024-10-19 14:43:05 +0800

    Update authRouter.js

--- Router/authRouter.js
+++ Router/authRouter.js
@@ -5,14 +5,14 @@ import * as user from "../controllers/userControllers.js";
 const Route = express.Router(); // Menggunakan express.Router()
 
 // Rute untuk '/register'
-Route.post("/register", Auth.usersRegistration);
+Route.post("/data/register", Auth.usersRegistration);
 
 // Rute untuk '/login'
-Route.post("/login", Auth.usersLogin);
+Route.post("/data/login", Auth.usersLogin);
 
 Route.post("/logout", protectedMiddleware, Auth.userLogout);
 
 // Mengambil data user
-Route.post("/users", protectedMiddleware, user.getCurrentUser);
+Route.post("/data/users", protectedMiddleware, user.getCurrentUser);
 // Ekspor router
 export default Route;
